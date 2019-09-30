19e5647222ba06dbbcd02a9f06f15e4f888333c2
Rudrik Patel <rudrik.patel@zymr.com>
2019-09-30 15:40:24 +0530

    Natours -- Implemented the multiple middle ware in same request so that it will execute sequentially. -- Implemented the parameterized middleware so that it will execute only on certain condition.

--- DemoSample/app.js
+++ DemoSample/app.js
@@ -8,8 +8,8 @@ const app = express();
 
 //1) Middle Ware.  This is middle ware that will apply to each and every request.
 app.use(morgan('dev'));
-
 app.use(express.json());
+app.use(express.static(`${__dirname}/public`));
 
 app.use((req, res, next) => {
     console.log('Hello from the middleware');
--- DemoSample/controllers/tourController.js
+++ DemoSample/controllers/tourController.js
@@ -2,6 +2,28 @@ const fs = require('fs');
 
 const tours = JSON.parse(fs.readFileSync(`${__dirname}/../dev-data/data/tours-simple.json`));
 
+
+exports.checkID = (req, res, next, val) => {
+    console.log(`Tour id is ${val}`);
+    if (req.params.id * 1 > tours.length) {
+        return res.status(404).json({
+            status: 'fail',
+            message: 'Invalid ID'
+        });
+    }
+    next();
+}
+
+exports.checkBody = (req, res, next) => {
+    if (!req.body.name || !req.body.price) {
+        return res.status(400).json({
+            status: 'fail',
+            message: 'Missing name or price!'
+        });
+    }
+    next();
+}
+
 exports.getAllTours = (req, res) => {
     console.log(req.requestTime);
     res.status(200).json({
@@ -15,12 +37,6 @@ exports.getAllTours = (req, res) => {
 };
 
 exports.deleteTour = (req, res) => {
-    if (req.params.id * 1 > tours.length) {
-        return res.status(404).json({
-            status: 'fail',
-            message: 'Invalid ID'
-        });
-    }
     res.status(204).json({
         status: 'success',
         data: null
@@ -31,13 +47,6 @@ exports.getTour = (req, res) => {
     console.log(req.params);
     const id = req.params.id * 1;
     const tour = tours.find(el => el.id === id);
-    // if (id > tours.length) {
-    if (!tour) {
-        return res.status(404).json({
-            status: 'fail',
-            message: 'Invalid ID'
-        });
-    }
     res.status(200).json({
         status: 'success',
         data: {
@@ -52,7 +61,7 @@ exports.createTour = (req, res) => {
     const newTour = Object.assign({ id: newId }, req.body);
 
     tours.push(newTour);
-    fs.writeFile(`${__dirname}/dev-data/data/tours-simple.json`, JSON.stringify(tours), (err) => {
+    fs.writeFile(`${__dirname}`+ '/../dev-data/data/tours-simple.json', JSON.stringify(tours), (err) => {
         res.status(201).json({
             status: 'success',
             data: {
@@ -63,12 +72,6 @@ exports.createTour = (req, res) => {
 };
 
 exports.updateTour = (req, res) => {
-    if (req.params.id * 1 > tours.length) {
-        return res.status(404).json({
-            status: 'fail',
-            message: 'Invalid ID'
-        });
-    }
     res.status(200).json({
         status: 'success',
         data: {
