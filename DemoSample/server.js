4b265a539efe5ae5935738079147803e3af4ea49
Rudrik Patel <rudrik.patel@zymr.com>
2019-10-03 11:52:47 +0530

    Natours -- Added Mongoose for the MongoDB -- Added DB path into the config.env file. -- Connect to the database in server.js file using the mongoose using the DB path and password of the DB.

--- DemoSample/server.js
+++ DemoSample/server.js
@@ -1,8 +1,22 @@
+const mongoose = require('mongoose');
 const dotenv = require('dotenv');
 
 dotenv.config({ path: './config.env' });
 
+const DB = process.env.DATABASE.replace(
+  '<PASSWORD>',
+  process.env.DATABASE_PASSWORD
+);
+
+mongoose
+  .connect(DB, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useFindAndModify: false
+  })
+  .then(() => console.log('DB Connection successfll'));
 const app = require('./app');
+
 // console.log(process.env);
 
 const port = process.env.PORT || 3000;
