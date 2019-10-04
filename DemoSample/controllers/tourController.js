2677999a25cfecbdadcbf7bc3a4f3b5176e22a9e
Rudrik Patel <rudrik.patel@zymr.com>
2019-10-04 16:28:20 +0530

    Natours -- Implemented the parameterized query. -- Excluding the field from the object. -- Fetch the new query object using the destructring.

--- DemoSample/controllers/tourController.js
+++ DemoSample/controllers/tourController.js
@@ -13,7 +13,31 @@ const Tour = require('./../models/tourModel');
 
 exports.getAllTours = async (req, res) => {
   try {
-    const tours = await Tour.find();
+    //BUILDD QUERY
+    // eslint-disable-next-line node/no-unsupported-features/es-syntax
+    const queryObj = { ...req.query };
+    const excludedFields = ['page', 'sort', 'limit', 'fields'];
+
+    excludedFields.forEach(el => delete queryObj[el]);
+
+    console.log(req.query, queryObj);
+
+    // const tours = await Tour.find({
+    //   duration: 5,
+    //   difficulty: 'easy'
+    // });
+    const query = Tour.find(queryObj);
+
+    //EXECUTE QUERY
+    const tours = await query;
+
+    // const tours = await Tour.find()
+    //   .where('duration')
+    //   .equals(5)
+    //   .where('difficulty')
+    //   .equals('easy');
+
+    // SEND RESPONSE
     res.status(200).json({
       status: 'success',
       results: tours.length,
