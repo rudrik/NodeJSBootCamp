c62163f909100595f0f96a78ba2bac31be5b2d0c
Rudrik Patel <rudrik.patel@zymr.com>
2019-10-09 12:35:50 +0530

    Natours -- Implemented the alias route. -- Using the middle ware in the get all tour, implemented the alias route.

--- DemoSample/controllers/tourController.js
+++ DemoSample/controllers/tourController.js
@@ -11,6 +11,20 @@ const Tour = require('./../models/tourModel');
 //   next();
 // };
 
+exports.aliasTopTours = async (req, res, next) => {
+  try {
+    req.query.limit = '5';
+    req.query.sort = '-ratingsAverage,price';
+    req.query.fields = 'name,price,ratingsAverage,summary,difficulty';
+    next();
+  } catch (err) {
+    res.status(404).json({
+      status: 'fail',
+      message: err
+    });
+  }
+};
+
 exports.getAllTours = async (req, res) => {
   try {
     console.log(req.query);
