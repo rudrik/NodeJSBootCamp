bf16cbee50941d089ac2a02ab42bd65e5c5440eb
Rudrik Patel <rudrik.patel@zymr.com>
2019-10-10 11:51:30 +0530

    Natours -- Implemneted the middle ware for document that will run before save and create method -- Added slugify to implement the slugify for the name before saving the document.

--- DemoSample/models/tourModel.js
+++ DemoSample/models/tourModel.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose');
+const slugify = require('slugify');
 
 const tourSchema = new mongoose.Schema(
   {
@@ -7,6 +8,9 @@ const tourSchema = new mongoose.Schema(
       required: [true, 'A tour must have a name'],
       unique: true,
       trim: true
+    },
+    slug: {
+      type: String
     },
     duration: {
       type: Number,
@@ -59,6 +63,21 @@ const tourSchema = new mongoose.Schema(
     toObject: { virtuals: true }
   }
 );
+//DOCUMENT MIDDLEWARE: runs before the .save() and the .create()
+tourSchema.pre('save', function(next) {
+  this.slug = slugify(this.name, { lower: true });
+  next();
+});
+
+// tourSchema.pre('save', function(next) {
+//   console.log('Will save document');
+//   next();
+// });
+
+// tourSchema.post('save', function(doc, next) {
+//   console.log(doc);
+//   next();
+// });
 
 tourSchema.virtual('durationWeeks').get(function() {
   return this.duration / 7;
