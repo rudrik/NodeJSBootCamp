f737a328d1edf73870ef038228c868563f8b603d
Rudrik Patel <rudrik.patel@zymr.com>
2019-10-10 17:06:32 +0530

    Natours -- Implemented the Built in validator in the mongoose schema model.

--- DemoSample/controllers/tourController.js
+++ DemoSample/controllers/tourController.js
@@ -106,7 +106,8 @@ exports.createTour = async (req, res) => {
 exports.updateTour = async (req, res) => {
   try {
     const tour = await Tour.findByIdAndUpdate(req.params.id, req.body, {
-      new: true
+      new: true,
+      runValidators: true
     });
 
     res.status(200).json({
