89adef27940740dc1a656919d45b5938f0bc0a56
Rudrik Patel <rudrik.patel@zymr.com>
2019-10-16 11:21:45 +0530

    Natours -- Implemented the 404 error in the respected async function.

--- DemoSample/controllers/tourController.js
+++ DemoSample/controllers/tourController.js
@@ -1,6 +1,7 @@
 const Tour = require('./../models/tourModel');
 const APIFeatures = require('./../utils/apiFeatures');
 const catchAsync = require('./../utils/catchAsync');
+const AppError = require('./../utils/appError');
 // exports.checkID = (req, res, next, val) => {
 //   console.log(`Tour id is ${val}`);
 //   if (req.params.id * 1 > tours.length) {
@@ -45,7 +46,11 @@ exports.getAllTours = catchAsync(async (req, res, next) => {
 });
 
 exports.deleteTour = catchAsync(async (req, res, next) => {
-  await Tour.findByIdAndDelete(req.params.id);
+  const tour = await Tour.findByIdAndDelete(req.params.id);
+
+  if (!tour) {
+    return next(new AppError('No tour found with that ID', 404));
+  }
   res.status(204).json({
     status: 'success',
     data: null
@@ -55,6 +60,10 @@ exports.deleteTour = catchAsync(async (req, res, next) => {
 exports.getTour = catchAsync(async (req, res, next) => {
   const tour = await Tour.findById(req.params.id);
   // Tour.findOne({ _id: req.params.id})
+
+  if (!tour) {
+    return next(new AppError('No tour found with that ID', 404));
+  }
   res.status(200).json({
     status: 'success',
     data: {
@@ -78,7 +87,9 @@ exports.updateTour = catchAsync(async (req, res, next) => {
     new: true,
     runValidators: true
   });
-
+  if (!tour) {
+    return next(new AppError('No tour found with that ID', 404));
+  }
   res.status(200).json({
     status: 'success',
     data: {
