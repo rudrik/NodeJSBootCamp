b9e23e995eccfaffdbe9bc3bd7a702f7ac843a54
Rudrik Patel <rudrik.patel@zymr.com>
2019-10-31 11:16:45 +0530

    Natours -- Added methods in Auth Controller for forgot and reset password. -- Added the route for the forgot password. -- Added password reset token with expiration in the user model.

--- DemoSample/routes/userRoutes.js
+++ DemoSample/routes/userRoutes.js
@@ -8,6 +8,9 @@ const router = express.Router();
 router.post('/login', authController.login);
 router.post('/signUp', authController.signUp);
 
+router.post('/forgotPassword', authController.forgotPassword);
+router.post('/resetPassword', authController.resetPassword);
+
 router
   .route('/')
   .get(userController.getAllUsers)
---
+++ DemoSample/utils/email.js
@@ -0,0 +1,7 @@
+const nodemailer = require('nodemailer');
+
+const sendEmail = options => {
+  //1) Create a transporter
+  // 2) define the email options
+  // 3) Send the email
+};
