2c45c629af0e6eb8d2e2b416e80d95b497c1b666
Rudrik Patel <rudrik.patel@zymr.com>
2019-11-07 17:31:08 +0530

    Natours -- Implemented embadded guide data in tour model. Which we will change to reference model in later video.

--- DemoSample/models/tourModel.js
+++ DemoSample/models/tourModel.js
@@ -1,5 +1,6 @@
 const mongoose = require('mongoose');
 const slugify = require('slugify');
+const User = require('./userModel');
 // const validator = require('validator');
 
 const tourSchema = new mongoose.Schema(
@@ -104,7 +105,8 @@ const tourSchema = new mongoose.Schema(
         description: String,
         day: Number
       }
-    ]
+    ],
+    guides: Array
   },
   {
     toJSON: { virtuals: true },
@@ -114,6 +116,12 @@ const tourSchema = new mongoose.Schema(
 //DOCUMENT MIDDLEWARE: runs before the .save() and the .create()
 tourSchema.pre('save', function(next) {
   this.slug = slugify(this.name, { lower: true });
+  next();
+});
+
+tourSchema.pre('save', async function(next) {
+  const guidesPromises = this.guides.map(async id => await User.findById(id));
+  this.guides = await Promise.all(guidesPromises);
   next();
 });
 
