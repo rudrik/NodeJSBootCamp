f4a5dd98acb49e76fccda2964211e9e6e09a7311
Rudrik Patel <rudrik.patel@zymr.com>
2019-11-07 17:02:38 +0530

    Natours -- Implemented the Embadded location in the tour data model. -- Imported the data into the Tour collection.

--- DemoSample/dev-data/data/import-dev-data.js
+++ DemoSample/dev-data/data/import-dev-data.js
@@ -19,9 +19,7 @@ mongoose
   .then(() => console.log('DB Connection successfll'));
 
 //READ JSON FILE
-const tours = JSON.parse(
-  fs.readFileSync(`${__dirname}/tours-simple.json`, 'utf-8')
-);
+const tours = JSON.parse(fs.readFileSync(`${__dirname}/tours.json`, 'utf-8'));
 
 //IMORT DATA INTO DATABASE
 const importData = async () => {
--- DemoSample/models/tourModel.js
+++ DemoSample/models/tourModel.js
@@ -79,7 +79,32 @@ const tourSchema = new mongoose.Schema(
     secretTour: {
       type: Boolean,
       default: false
-    }
+    },
+    startLocation: {
+      // GeoJSON
+      type: {
+        type: String,
+        default: 'Point',
+        enum: ['Point']
+      },
+      coordinates: [Number],
+      address: String,
+      description: String
+    },
+    // Embadded document need to be array in order to create it.
+    locations: [
+      {
+        type: {
+          type: String,
+          default: 'Point',
+          enum: ['Point']
+        },
+        coordinates: [Number],
+        address: String,
+        description: String,
+        day: Number
+      }
+    ]
   },
   {
     toJSON: { virtuals: true },
