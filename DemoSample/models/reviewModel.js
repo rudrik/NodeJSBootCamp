f1a8413ab1185be456de07b45e269c3a12c479c1
Rudrik Patel <rudrik.patel@zymr.com>
2019-11-26 17:41:52 +0530

    Natours -- Implemented the Average rating and number of review inside the review model when saving the review. -- Updated the tour model as per the ratings we received.

--- DemoSample/models/reviewModel.js
+++ DemoSample/models/reviewModel.js
@@ -49,7 +49,7 @@ reviewSchema.pre(/^find/, function(next) {
 });
 
 reviewSchema.statics.calcAverageRatings = async function(tourId) {
-  console.log(tourId);
+  // console.log(tourId);
   const stats = await this.aggregate([
     {
       $match: { tour: tourId }
@@ -62,7 +62,7 @@ reviewSchema.statics.calcAverageRatings = async function(tourId) {
       }
     }
   ]);
-  console.log(stats);
+  // console.log(stats);
 
   if (stats.length > 0) {
     await Tour.findByIdAndUpdate(tourId, {
@@ -84,7 +84,7 @@ reviewSchema.post('save', function() {
 
 reviewSchema.pre(/^findOneAnd/, async function(next) {
   this.r = await this.findOne();
-  console.log(this.r);
+  // console.log(this.r);
   next();
 });
 
